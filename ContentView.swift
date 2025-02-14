025c811f644f398b701a47b44457e51d02f43274
Teniel Smith-Edwards <tsmith1227@outlook.com>
2025-02-13 22:17:59 -0500

    Textbox created

--- ContentView.swift
+++ ContentView.swift
@@ -1,12 +1,19 @@
 import SwiftUI
 
 struct ContentView: View {
+    // state to declare num
+    @State private var num = 0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("\(num)")
+            
+            
         }
     }
+}
+
+
+#Preview {
+    ContentView()
 }
